62d6c78e3cc245ae18c9a1bce243fc4c580e3b19
Karamoshi Nakatoto <karamoshi.nakatoto@protonmail.com>
2023-02-19 17:42:47 +0200

    feat(gcloud): update provisioning script and default values

--- gcloud.sh
+++ gcloud.sh
@@ -1,10 +1,13 @@
 # shellcheck shell=bash
 
+# This script creates a free-tier compatible VM with your credentials
+
+# Add these arguments for spot instances:
+#     --provisioning-model=SPOT \
+#     --instance-termination-action=STOP \
 gcloud beta compute instances create instance-1 \
-    --provisioning-model=SPOT \
-    --instance-termination-action=STOP \
     --image-family=debian-10 \
     --image-project=debian-cloud \
-    --metadata=ssh-keys=karamoshi:"$(cat ~/.ssh/id_rsa.pub)" \
-    --machine-type=e2-highcpu-4 \
-    --zone=us-central1-c
+    --metadata=ssh-keys="$(whoami):$(cat ~/.ssh/id_rsa.pub)" \
+    --machine-type=e2-micro \
+    --zone=us-central1-a
